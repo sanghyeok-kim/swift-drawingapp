ece84fda9c0ef025ffb65fcd547ae821bf668d99
sanghyeok-kim <slsnsi1212@gmail.com>
2022-03-10 16:25:35 +0900

    [Feat] Shapable 프로토콜에 객체의 UIColor와 Frame값 반환해주는 함수 extension으로 구현

--- DrawingApp/DrawingApp/Model/Shapable.swift
+++ DrawingApp/DrawingApp/Model/Shapable.swift
@@ -6,16 +6,40 @@
 //
 
 import Foundation
+import UIKit
 
 protocol Shapable: CustomStringConvertible {
     var id: Identifier { get }
     var size: Size { get }
     var point: Point { get }
     var backGroundColor: Color { get }
+    var alpha: Alpha { get }
 }
 
 extension Shapable {
     var description: String {
         return "(\(self.id)), \(self.point), \(self.size), \(self.backGroundColor)"
+    }
+}
+
+extension Shapable {
+    func getUIColor() -> UIColor {
+        let red = backGroundColor.red / Color.Range.upper
+        let green = backGroundColor.green / Color.Range.upper
+        let blue = backGroundColor.blue / Color.Range.upper
+        let alpha = alpha.value
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    func getShapeFrame() -> CGRect {
+        let xCoordinate = point.x
+        let yCoordinate = point.y
+        
+        let width = size.width
+        let height = size.height
+        
+        let frame = CGRect(x: xCoordinate, y: yCoordinate, width: width, height: height)
+        return frame
     }
 }
